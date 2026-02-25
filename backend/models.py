2980b3f3767cb332ddddc815fb27cec46504bfca
Kamalakar <kamalakarkagalkar@gmail.com>
2026-02-25 12:15:21 +0530

    second commit

---
+++ backend/models.py
@@ -0,0 +1,32 @@
+import uuid
+
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email_id = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    role = Column(String, default="staff")  # staff or admin
+    verified = Column(Boolean, default=False)
+    
+    products = relationship("Product", back_populates="user")
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+    quantity = Column(Integer, default=0)
+    user_id = Column(Integer, ForeignKey("users.id"), index=True)
+    
+    user = relationship("User", back_populates="products")
